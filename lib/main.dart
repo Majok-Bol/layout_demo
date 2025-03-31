c621b55a9412e6fbadd03546af884394d4b95582
Majok-Bol <bolmajok001@gmail.com>
2025-03-31 12:25:00 +0300

    added ButtonSection to the scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         appBar:AppBar(title:const Text(appTitle,style: TextStyle(color: Colors.red),),centerTitle: true,),
         // backgroundColor:Colors.grey,
-        body:const SingleChildScrollView(child: Column(children: [TitleSection(name: "Oeschinen Lake Campground", location:"Kandersteg, Switzerland")],),),
+        body:const SingleChildScrollView(child: Column(children: [TitleSection(name: "Oeschinen Lake Campground", location:"Kandersteg, Switzerland"),ButtonSection()],),),
       ),
     );
   }
